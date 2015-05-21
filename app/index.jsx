75eeef1bef6ec8cba925f7dd3356214db7c7a409
patw <patw.hi@gmail.com>
2015-05-21 16:42:26 +0800

    Refine demo page

--- app/index.jsx
+++ app/index.jsx
@@ -3,4 +3,4 @@
 var React = require('react');
 var TWzipcode = require('./main');
 
-React.render(<TWzipcode />, document.querySelector('.main'));
+React.render(<TWzipcode css={['form-control county-sel', 'form-control district-sel', 'form-control zipcode']} />, document.querySelector('.main'));
